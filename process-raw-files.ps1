479f0545f12c3d3e9152a942111d989b31dcc4d5
Andre Greeff <little.grief@gmail.com>
2022-10-23 02:07:01 +0200

    refactor: rework "dashcam set" rule files handling

--- process-raw-files.ps1
+++ process-raw-files.ps1
@@ -6,122 +6,115 @@
 # push-location $ProcessDir
 
 try {
+  # ! here we verify the current runtime environment..
+  # basically, if nothing kills the block, then we're good to go..
+  write-host ":: checking environment ::"
+
+  # * this folder holds the incoming video files, as copied directly from the SD card..
   $SourceDir = resolve-path ".\0-dashcam-source"
+  write-host "[env] SourceDir: $SourceDir"
+
+  # * this folder holds "video sets" as text files with CSV-style "rules" for cutting clips from source video files..
   $WorkingDir = resolve-path ".\1-working"
-  $OutputDir = resolve-path ".\2-rewrapped"
+  
+  $DashSetFiles = @(get-childitem "$WorkingDir\*.set")
+  $DashSetFileCount = $DashSetFiles.Count
+  write-host "[env] WorkingDir: $WorkingDir, with $DashSetFileCount dashcam video sets"
 
-  $DashcamSessions = get-childitem "$SourceDir\*"
-
-  foreach ($SetSourcePath in $DashcamSessions) {
-    $SetName = split-path $SetSourcePath -LeafBase
-    write-host ":: START :: $SetName ::"
-
-    $SetIsDir = test-path $SetSourcePath -PathType Container
-
-    if ($SetIsDir -eq $False) {
-      write-warning "!! this needs to be restructured as a 'session set'.."
-    }
-
-    $SetHasPartsFile = test-path "$SetSourcePath\parts" -PathType Leaf
-    if ($SetHasPartsFile -eq $False) {
-      write-error "!! this 'session set' has no parts file.."
-    }
-
-    $SessionFinalOutputPath = "$OutputDir\$SetName"
-    $SessionFinalOutputFile = "$SessionFinalOutputPath\concatenated.mov"
-
-    $SetNeedsProcessing = -not (test-path "$SessionFinalOutputFile" -PathType Leaf)
-    if ($SetNeedsProcessing -eq $False) {
-      write-error "!! this 'session set' has already been processed.."
-    }
-
-    $SetNameValid = $SetSourcePath -match '\\\d{4}-\d{2}-\d{2}\s-\s'
-
-    if ($SetNameValid) {
-      if (@($SetHasPartsFile, $SetNeedsProcessing) -notcontains $False) {
-        $SetWorkingDir = "$WorkingDir\$SetName"
-
-        # ! create a temporary working directory for this session set
-        if (test-path "$SetWorkingDir" -PathType Container) {
-          remove-item "$SetWorkingDir" -Force -Recurse
-        }
-        new-item "$SetWorkingDir" -ItemType Container | out-null
-        $SetLocalPartsPath = ".\parts.local"
-
-        push-location $SetWorkingDir
-
-        if ($SetIsDir -eq $False) {
-          # * NOTE: $SetFiles is an array of FileInfo objects
-          # my dashcam always saves files as AVI..
-          # $SetFiles = @(get-childitem "$SetSourcePath" -Filter '.avi')
-
-          # ! now cut the configured parts out of the source session file(s)
-          write-host "splitting raw dashcam files into defined parts.."
-
-          $SetIndexContent = get-content "$SetSourcePath\parts"
-          $PartCount = 0
-          foreach ($line in $SetIndexContent) {
-            $rule = $line.Split(",")
-
-            if ($rule.Count -ne 3) {
-              write-warning "file parsing rule should have 3 elements, even if the 2nd or 3rd are empty: $rule"
-            } else {
-              $ClipBaseName = $rule[0]
-              $ClipStart = $rule[1]
-              $ClipEnd = $rule[2]
-              $PartName = "part-$PartCount.mov"
-
-              $TrimCommand = "ffmpeg.exe -loglevel 16 -n -i `"$SetSourcePath\$ClipBaseName.avi`" -c copy"
-              # if excluded, clip "start" will NOT be trimmed
-              if ($ClipStart) { $TrimCommand += " -ss $ClipStart" }
-              # if excluded, clip "end" will NOT be trimmed
-              if ($ClipEnd) { $TrimCommand += " -to $ClipEnd" }
-
-              $TrimCommand += " '$PartName'"
-              write-host "trimming '$PartName'"
-              write-warning $TrimCommand
-              # invoke-expression $TrimCommand
-              write-output "file $PartName" >> "$SetLocalPartsPath"
-
-              $PartCount += 1
-            }
-          }
-        }
-
-        # ! then concatenate the rewrapped files
-        if (test-path "$SetLocalPartsPath" -PathType Leaf) {
-          write-host "concatenating dashcam part files into one.."
-
-          if (-not (test-path "$SessionFinalOutputPath" -PathType Container)) {
-            new-item "$SessionFinalOutputPath" -ItemType Container | out-null
-          }
-
-          $ConcatParts = get-content "$SetLocalPartsPath"
-          write-host "concatenating '$ConcatParts'"
-
-          $ConcatCommand = "ffmpeg.exe -loglevel 16 -n -f concat -i `"$SetLocalPartsPath`" -c copy `"$SessionFinalOutputFile`""
-          write-warning $ConcatCommand
-          # invoke-expression $ConcatCommand
-        }
-
-        pop-location
-      }
-
-      # ! final cleanup
-      if (test-path "$SessionFinalOutputFile" -PathType Leaf) {
-        if ((get-item "$SessionFinalOutputFile").length -gt 0kb) {
-          write-warning "cleaning up files..."
-          # remove-item $SetWorkingDir -Force -Recurse
-          # remove-item $SetSourcePath -Force -Recurse
-        }
-      }
-    }
-
-    write-host ":: FINISH :: $SetName ::"
-  }
+  # * this folder holds the out of "rewrapping" the clips extracted from the source video files..
+  $RewrappedDir = resolve-path ".\2-rewrapped"
+  write-host "[env] RewrappedDir: $RewrappedDir"
 } catch {
-  write-error "hmm.. something died uncomfortably: $_"
-  write-error $_.ScriptStackTrace
-} finally {
-  pop-location
+  write-error "hmm.. $_"
+  # write-error $_.ScriptStackTrace
+  exit 0
+}
+
+# ! [WIP] subject to change..
+# * ".set" files are basic text, containing one line per "source rule"
+# * each rule defined as "file_path,clip_start,clip_end"
+# * - 'file_path' is relative to $SourceDir
+# * - 'clip_start' is the number of seconds from the source file start
+# * - 'clip_end' is the number of seconds from the source file start
+
+$FfmpegCommand = "ffmpeg.exe -loglevel 16 -n"
+
+foreach ($file in $DashSetFiles) {
+  $DashSetName = split-path $file -LeafBase
+  try {
+    $SetWorkingDir = "$WorkingDir\$DashSetName"
+    
+    $SetOutputDir = "$RewrappedDir\$DashSetName"
+    if (-not (test-path "$SetOutputDir" -PathType Container)) {
+      new-item "$SetOutputDir" -ItemType Container | out-null
+    }
+
+    $SetOutputFile = "$SetOutputDir\concatenated.mov"
+    if (-not (test-path "$SetOutputFile" -PathType Leaf)) {
+      write-host ":: $DashSetName ::"
+
+      new-item "$SetWorkingDir" -ItemType Container | out-null
+      push-location "$SetWorkingDir"
+
+      $SetLocalPartsPath = "$SetWorkingDir\parts.local"
+      if (test-path "$SetLocalPartsPath" -PathType Leaf) {
+        remove-item "$SetLocalPartsPath" -Force
+      }
+
+      $count = 0
+      foreach ($entry in @(get-content $file)) {
+        write-host ":: checking set rules.. ::"
+        $rule = @($entry.Split(","))
+
+        $ClipSourceFile = $rule[0]
+        $ClipStart = 1 * $rule[1]
+        $ClipEnd = 1 * $rule[2]
+        $ClipStartString = ($ClipStart -ne 0) ? "${ClipStart}s" : "start"
+        $ClipEndString = ($ClipEnd -ne 0) ? "${ClipEnd}s" : "end"
+
+        write-host "testing file '$SourceDir\$ClipSourceFile'.."
+        if (-not (test-path "$SourceDir\$ClipSourceFile" -PathType Leaf)) {
+          write-error "missing source file: $SourceDir\$ClipSourceFile"
+          continue
+        }
+
+        if (($ClipEnd -ne 0) -eq ($ClipEnd -lt $ClipStart)) {
+          write-error "clip end time '$ClipEnd' is before clip start time '$ClipStart'"
+          continue
+        }
+
+        write-host ":: clipping parts from source files ::"
+        $count += 1
+        $PartName = "part-$count.mov"
+        $TrimCommand = "$FfmpegCommand -i `"$SourceDir\$ClipSourceFile`" -c copy"
+        if ($ClipStart -gt 0) { $TrimCommand += " -ss $ClipStart" }
+        if ($ClipEnd -gt 0) { $TrimCommand += " -to $ClipEnd" }
+        $TrimCommand += " '$PartName'"
+        write-warning $TrimCommand
+        invoke-expression $TrimCommand
+        write-output "file $PartName" >> "$SetLocalPartsPath"
+      }
+
+      write-host ":: concatenating clip parts.. ::"
+      $ConcatCommand = "$FfmpegCommand -f concat -i `"$SetLocalPartsPath`" -c copy `"$SetOutputFile`""
+      write-warning $ConcatCommand
+      invoke-expression $ConcatCommand
+      pop-location
+    }
+  } catch {
+    write-error "but.. $_"
+  } finally {
+    $HasSetWorkingDir = (test-path "$SetWorkingDir" -PathType Container)
+    # write-host "HasSetWorkingDir: $HasSetWorkingDir"
+    $HasSetOutputFile = (test-path "$SetOutputFile" -PathType Leaf)
+    # write-host "HasSetOutputFile: $HasSetOutputFile"
+    $SetOutputFileValid = ((get-item "$SetOutputFile").length -gt 0kb)
+    # write-host "SetOutputFileValid: $SetOutputFileValid"
+    if (@($HasSetWorkingDir, $HasSetOutputFile, $SetOutputFileValid) -notcontains $False) {
+      write-host ":: cleaning up.. ::"
+      remove-item "$SetWorkingDir" -Force -Recurse
+    }
+  }
+
+  # write-host " "
 }
