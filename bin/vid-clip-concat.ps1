5ab79cc31de94d2dd8456f9ed47b89af6b6659e3
Andre Greeff <little.grief@gmail.com>
2022-11-05 01:03:24 +0200

    refactor(DNM): rewrite video clipper script
    this is a massive refactor, and it needs a little more separation before final commit..

--- bin/vid-clip-concat.ps1
+++ bin/vid-clip-concat.ps1
@@ -1,10 +1,17 @@
-# * process all "dashcam set rules" as read from `.\1-working\`
+# * extract specified clips from source video files, creating a single concatenated output file.
+#
+# personally, I have two common uses for this script:
+#
+# - collecting dashcam incident footage from 2-minute revolving video clips
+# - clipping and/or joining gameplay screen recording sessions
+#
 # ! [WIP] behaviour subject to change without notice..
 #
 # for the monent:
+#
 # - ".set" files are basic text, containing one line per "source rule"
 # - each rule defined as "{file_path},{clip_start}-{clip_end},{extra_step}"
-#   - '{file_path}' is relative to $SourceDir
+#   - '{file_path}' is relative to $VideosPath
 #   - '{clip_start}-{clip_end}' is ONE FIELD with two video seeker positions of the clip to copy
 #     - * to incude the whole file, leave this blank or pass in '-'
 #     - * to cut from a set position _to the end_, set this as '{clip_start}-'
@@ -12,139 +19,127 @@
 #   - '{extra_step}' [WIP] think of this as a "post-process hook", but currently it serves one solitary purpose:
 #     - to allow 'REPAIR' to be passed in, which moves the source file to a special folder.. :/
 
-$IS_DRY_RUN = (@(($args -contains "--exec"),($args -contains "-x") -notcontains $True))
-if ($IS_DRY_RUN) {
-  write-warning "going --dry-run style.. no changes will be made."
+param(
+  # ! this is the "video clip rules list" project file to work with.. so, yes, it's mandatory.
+  [Parameter(Mandatory)] [Alias("l")] [string] $ListFile,
+
+  # ? (optional) a resolvable path to use as the "root path" for any relative video clip rule file paths.
+  # if not set, this script will use the current process working directory by default.
+  # this has no effect on absolute video clip rule file paths.
+  [Parameter()] [Alias("v")] [string] $Videos = ".",
+
+  # ? (optional) a resolvable path to use as the "working directory" for working with the extracted video clip(s).
+  # if not set, this defaults to a folder named after your project, within your host OS "TEMP" folder.
+  [Parameter()] [Alias("t")] [string] $Temp = "$Env:TEMP",
+
+  # ? (optional) a resolvable path to use as the "output folder" for the final concatenated video file(s).
+  # if not set, this script will use the current working folder by default.
+  [Parameter()] [Alias("o")] [string] $Output = ".",
+
+  # ? (optional) setting this will prevent the creation of video project output subfolders.
+  # unless explicitly set, the concatenated output video file will be named after its project.
+  [Parameter()] [Alias("f")] [switch] $Flat = $False
+)
+
+Write-Debug "[ListFile] $ListFile"
+Write-Debug "[Videos] $Videos"
+Write-Debug "[Temp] $Temp"
+Write-Debug "[Output] $Output"
+Write-Debug "[Flat] $Flat"
+
+# ! verify the current runtime environment..
+try {
+  $FfmpegVersion = (($(Invoke-Expression "ffmpeg -version" -ErrorAction Stop) -split "\n")[0] -split "\s")[2]
+  Write-Verbose "[FfmpegVersion] $FfmpegVersion"
+
+  $ListFilePath = Resolve-Path "$ListFile" -ErrorAction Stop
+  Write-Verbose "[ListFilePath] resolved as: $ListFilePath"
+
+  $VideosPath = Resolve-Path "$Videos" -ErrorAction Stop
+  Write-Verbose "[VideosPath] ($(($PSBoundParameters.ContainsKey("Videos")) ? "custom!!" : "default..")) resolved as: $VideosPath"
+
+  $TempPath = Resolve-Path "$Temp" -ErrorAction Stop
+  Write-Verbose "[TempPath] ($(($PSBoundParameters.ContainsKey("Temp")) ? "custom!!" : "default..")) resolved as: $TempPath"
+
+  $OutputPath = Resolve-Path "$Output" -ErrorAction Stop
+  Write-Verbose "[OutputPath] ($(($PSBoundParameters.ContainsKey("Output")) ? "custom!!" : "default..")) resolved as: $OutputPath"
 }
+catch {
+  Write-Error "oh dear.. $_"
+  Write-Debug $_.ScriptStackTrace
+  exit 1
+}
+
+Write-Debug "[FfmpegVersion] $FfmpegVersion"
+Write-Debug "[ListFilePath] $ListFilePath"
+Write-Debug "[VideosPath] $VideosPath"
+Write-Debug "[TempPath] $TempPath"
+Write-Debug "[OutputPath] $OutputPath"
 
 try {
-  # ! here we verify the current runtime environment..
-  # basically, if nothing kills the block, then we're good to go..
+  $ProjectName = Split-Path $ListFilePath -LeafBase
+  Write-Host ":: processing video project `"$ProjectName`".. ::"
 
-  $BackupDir = resolve-path ".\X-BACKUP" -ErrorAction Stop
+  $ListFileRules = @(Get-Content "$ListFilePath")
+  Write-Debug "ListFileRules: $ListFileRules"
+  Write-Verbose "found $($ListFileRules.Count) entries"
 
-  # * this folder holds the incoming video files, as copied directly from the SD card..
-  $SourceDir = resolve-path ".\0-sources" -ErrorAction Stop
-  $SourceNeedsRepairDir = "$BackupDir\needs-repair"
+  # write-warning $ListFilePath.Extension
+  # write-warning $ListFilePath.Directory
 
-  # * this folder holds "video sets" as text files with CSV-style "rules" for cutting clips from source video files..
-  $WorkingDir = resolve-path ".\1-working" -ErrorAction Stop
-  $SetFilesDoneDir = "$BackupDir\set-files"
+  $ProjectWorkingDir = "$TempPath\$ProjectName"
 
-  # * this folder holds the out of "rewrapping" the clips extracted from the source video files..
-  $RewrappedDir = resolve-path ".\2-rewrapped" -ErrorAction Stop
-} catch {
-  write-error "hmm.. $_"
-  # write-error $_.ScriptStackTrace
-  exit 0
-}
+  $RuleCounter = 0
+  foreach ($Rule in $ListFileRules) {
+    $RuleParts = @($Rule.Split(","))
 
-foreach ($SetFile in @(get-childitem "$WorkingDir\*.set")) {
-  # ! NOTE: here the try/catch is _inside_ the loop, because each set should have a chance at being used..
-  # ..regardless, ultimately there will be much refactoring.
-  try {
-    # write-warning $SetFile.Name
-    # write-warning $SetFile.BaseName
-    # write-warning $SetFile.Extension
-    # write-warning $SetFile.Directory
+    $ClipGroup = $RuleParts[0]
+    $ClipPath = $RuleParts[1]
+    $ClipRange = $RuleParts[2]
 
-    $DashSetName = $SetFile.BaseName
-    $SetFileName = $SetFile.Name
-    $SetWorkingDir = "$WorkingDir\$DashSetName"
-    $SetOutputDir = "$RewrappedDir\$DashSetName"
-    $SetOutputFile = "$SetOutputDir\concatenated.mov"
-    $SetLocalPartsPath = "$SetWorkingDir\parts.local"
+    $ClipGroupPartsPath = "$ProjectWorkingDir\$ClipGroup.parts"
 
-    if (-not (test-path "$SetOutputDir" -PathType Container)) {
-      new-item "$SetOutputDir" -ItemType Container | out-null
+    if (-not (test-path "$VideosPath\$ClipPath" -PathType Leaf)) {
+      write-error "source file missing: $VideosPath\$ClipPath"
+      continue
+    }
+    $RuleCounter += 1
+
+    $ClipRangeParts = @($ClipRange.Split("-"))
+    $ClipStart = 1 * $ClipRangeParts[0]
+    $ClipEnd = 1 * $ClipRangeParts[1]
+
+    $TrimCommand = "ffmpeg.exe -loglevel 16 -n -i `"$SourceDir\$ClipPath`" -c copy"
+    if ($ClipStart -gt 0) {
+      $TrimCommand = "$TrimCommand -ss $ClipStart"
+    }
+    if ($ClipEnd -gt 0) {
+      $TrimCommand = "$TrimCommand -to $ClipEnd"
     }
 
-    if (-not (test-path "$SetOutputFile" -PathType Leaf)) {
-      if (test-path "$SetWorkingDir" -PathType Container) {
-        remove-item "$SetWorkingDir" -Force -Recurse
-      }
-      new-item "$SetWorkingDir" -ItemType Container | out-null
-      push-location "$SetWorkingDir"
+    $PartName = "part-$RuleCounter.mov"
+    $TrimCommand = "$TrimCommand '$PartName'"
+    write-output "file $PartName" >> "$ClipGroupPartsPath"
 
-      if (test-path "$SetLocalPartsPath" -PathType Leaf) {
-        remove-item "$SetLocalPartsPath" -Force
-      }
-
-      write-host ":: processing rules for '$DashSetName'.. ::"
-
-      $ClipCounter = 0
-      foreach ($SetRule in @(get-content $SetFile)) {
-        $SetRuleParts = @($SetRule.Split(","))
-
-        $ClipPath = $SetRuleParts[0]
-        if (-not (test-path "$SourceDir\$ClipPath" -PathType Leaf)) {
-          write-error "source file missing: $SourceDir\$ClipPath"
-          continue
-        }
-        $ClipCounter += 1
-
-        $ClipRange = $SetRuleParts[1]
-        $ClipRangeParts = @($ClipRange.Split("-"))
-        $ClipStart = 1 * $ClipRangeParts[0]
-        $ClipEnd = 1 * $ClipRangeParts[1]
-
-        $TrimCommand = "ffmpeg.exe -loglevel 16 -n -i `"$SourceDir\$ClipPath`" -c copy"
-
-        if ($ClipStart -gt 0) {
-          $TrimCommand += " -ss $ClipStart"
-        }
-        if ($ClipEnd -gt 0) {
-          $TrimCommand += " -to $ClipEnd"
-        }
-
-        $PartName = "part-$ClipCounter.mov"
-        $TrimCommand += " '$PartName'"
-        write-output "file $PartName" >> "$SetLocalPartsPath"
-
-        if (-not $IS_DRY_RUN) {
-          invoke-expression $TrimCommand
-        } else {
-          write-warning "[--dry-run] $TrimCommand"
-        }
-
-        # ! this is simply going to slot in after the current process, for now.. but this must be integrated more carefully before being commited..
-        $SourceClipExtraStep = $SetRuleParts[2]
-        if ("$SourceClipExtraStep" -eq "REPAIR") {
-          if (-not $IS_DRY_RUN) {
-            write-warning "moving source clip to 'needs repair' folder.."
-            move-item -Path "$SourceDir\$ClipPath" -Destination "$SourceNeedsRepairDir\$ClipPath"
-          } else {
-            write-warning "[--dry-run] NOT moving source clip to 'needs repair' folder.."
-          }
-        }
-      }
-
-      write-host ":: concatenating clip parts.. ::"
-      $ConcatCommand = "ffmpeg.exe -loglevel 16 -n -f concat -i `"$SetLocalPartsPath`" -c copy `"$SetOutputFile`""
-
-      if (-not $IS_DRY_RUN) {
-        invoke-expression $ConcatCommand
-      } else {
-        write-warning "[--dry-run] $ConcatCommand"
-      }
-
-      # ! we can leave the set working directory now..
-      pop-location
-
-      # ! obligatory EOF cleanup..
-      write-host ":: runtime clean up.. ::"
-
-      if ((test-path "$SetOutputFile" -PathType Leaf)) {
-        # move the current set rules file..
-        move-item -Path "$WorkingDir\$SetFileName" -Destination "$SetFilesDoneDir\$SetFileName"
-      }
-
-      if ((test-path "$SetWorkingDir" -PathType Container)) {
-        # clear set process working directory
-        remove-item "$SetWorkingDir" -Force -Recurse
-      }
-    }
-  } catch {
-    write-error "wait, what?? $_"
+    invoke-expression $TrimCommand
   }
+
+  $ProjectOutputFolder = ($Flat -eq $true) ? "$RewrappedDir" : "$RewrappedDir\$ProjectName"
+  if (-not (test-path "$ProjectOutputFolder" -PathType Container)) {
+    new-item "$ProjectOutputFolder" -ItemType Container | out-null
+  }
+
+  $ProjectOutputFile = ($Flat -eq $true) ? "$ProjectName.mov" : "concatenated.mov"
+
+  write-host ":: concatenating clip parts.. ::"
+  foreach ($ClipGroupList in @(Get-ChildItem "$ProjectWorkingDir" -File -Filter *.parts)) {
+    $ConcatCommand = "ffmpeg.exe -loglevel 16 -n -f concat -i `"$ClipGroupList`" -c copy `"$ProjectOutputFile`""
+
+    invoke-expression $ConcatCommand
+  }
+}
+catch {
+  Write-Error "hmmm.. $_"
+  Write-Debug $_.ScriptStackTrace
+  exit 1
 }
